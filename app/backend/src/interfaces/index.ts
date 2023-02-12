595346b9cc4ed6bce984062d32d570ce1e08cd57
xWesleyMartins <wesley_869@hotmail.com.br>
2023-02-12 18:37:51 -0300

    corrigindo lint

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -1,8 +1,8 @@
 import ILogin from './login';
 import IUser from './user';
 import ICreateToken from './ICreateToken';
-import IMatches from './IMatches';
+// import IMatches from './IMatches';
 
-export { ILogin, IUser, ICreateToken, IMatches };
+export { ILogin, IUser, ICreateToken };
 // export { ILogin };
 // export { IUser };
